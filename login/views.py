27a173e7ce6f66947b12384aad44f230addcfef8
Arthur Arnx <arthur.arnx@gmail.com>
2021-06-29 17:23:29 +0200

    new connexion page + pwd

--- login/views.py
+++ login/views.py
@@ -2,23 +2,24 @@ from django.contrib.auth import authenticate
 import django.contrib.auth
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import NameForm
+from .forms import ConnexionForm
 from django.http import HttpResponse
 
 def login(request):
     request.session["name"] = None
     django.contrib.auth.logout(request)
     if request.method == 'POST':
-        form = NameForm(request.POST)
+        form = ConnexionForm(request.POST)
         if form.is_valid():
             request.session["name"] = form.cleaned_data.get("name").upper()
             name = request.session.get("name")
-            user = authenticate(username=name, password=f"{name.lower()}{name.lower()}")
+            pwd = form.cleaned_data.get("password")
+            user = authenticate(username=name, password=pwd)
             if user is not None :
                 django.contrib.auth.login(request, user)
                 return HttpResponseRedirect('/main/')
             if name == "ADMIN":
                 return HttpResponseRedirect('/control/')
     else:
-        form = NameForm()
+        form = ConnexionForm()
     return render(request, 'login/login.html', {'form':form})
